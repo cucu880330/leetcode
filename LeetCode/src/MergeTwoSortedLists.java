7d3cd558e600dba9142e97b80534697beb802372
Robin <eternal33b@gmail.com>
2022-08-24 00:35:40 -0400

    *Grind #3 Merge Two Sorted Lists

---
+++ LeetCode/src/MergeTwoSortedLists.java
@@ -0,0 +1,70 @@
+public class MergeTwoSortedLists {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public ListNode mergeTwoListsRecursive(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoListsRecursive(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoListsRecursive(list2.next, list1);
+            return list2;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val > l2.val) {
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+
+        ListNode res = l1;
+        while(l1!= null && l2!= null) {
+            ListNode curr = null;
+            while(l1 != null && l1.val <= l2.val) {
+                curr = l1;
+                l1 = l1.next;
+            }
+            curr.next = l2;
+            ListNode temp =l1;
+            l1= l2;
+            l2 = temp;
+        }
+        return res;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
